6236327309722fa2d9d04ac544aef8264b152474
Alexander Gräfenstein <giniedp@online.de>
2011-02-28 23:20:16 +0100

    -

--- lib/fancy_tools/git_helper.rb
+++ lib/fancy_tools/git_helper.rb
@@ -5,24 +5,28 @@ module FancyTools::GitInfo
   
   def git_info
     unless @@info
-      @@info = {
-        :remote_url => `git remote -v`,
-        :remote_branche => `git branch -r`,
-        :last_commit => `git log --max-count=1`
-      }
+      begin
+        @@info = {
+          :application => app_name = (Rails.application.class.to_s.split('::').first rescue ""),
+          :environment => Rails.env,
+          :remote_url => `git remote -v`,
+          :remote_branch => `git branch -r`,
+          :last_commit => `git log --max-count=1`
+        }
+      rescue
+        @@info = {}
+      end
     end
     @@info
   end
   
   def git_info_text
-    app_name = (Rails.application.class.to_s.split('::').first rescue "")
-    
     return git_info.map do |k, v|
       content_tag(:div) do
-        out = content_tag(:span, "#{k.to_s.humanize} :", :class => "strong")
+        out = content_tag(:span, "#{k.to_s.humanize} : ", :class => "strong")
         out.concat content_tag(:span, "#{v}")
       end
-    end
+    end.join()
   end
 end
 
