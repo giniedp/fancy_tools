861efad6d8a2b01b923ebd6f25bbad197a5f1c97
Alexander Gräfenstein <alex@empuxa.com>
2011-11-16 14:06:24 +0100

    improved the icon helper

--- lib/fancy_tools/icon_helper.rb
+++ lib/fancy_tools/icon_helper.rb
@@ -1,178 +1,121 @@
+require "active_support/ordered_hash"
+
 module FancyTools
   
-  # Webapplications always have lots of icons in its pages. This module helps to
-  # write them with a short command.
-  #
-  # === Example
-  #    
-  #    # writing this
-  #    named_icon(:new_icon)
-  #
-  #    # gives same result as writing this
-  #    image_tag("path/to/transparent/img.gif", :size=>"16x16", :class => :new_icon)
-  #
+  # Provides helper methods that help writing image tags in a shorter and more efficient command.
+  # NOTE: this image tag spits out an image with a transparent pixel. The styling must be done in the css depend in the class attribute
   module IconHelper
   
-    # Mapping of names to css class names
-    mattr_accessor :class_mapping 
-    @@class_mapping  = {
-      :action => {
-        :default => :action_icon,
-        :add => :add_icon,
-        :show => :show_icon,
-        :new => :new_icon,
-        :edit => :edit_icon,
-        :delete => :delete_icon,
-        :accept => :acceppt_icon,
-        :success => :accept_icon,
-        :reject => :cross_icon
-      },
-      
-      :mime => {
-        :default => :document_icon,
-        :application => {
-          :default => :document_icon,
-          :pdf => :pdf_icon,
-          :gzip => :archive_icon,
-          :zip => :archive_icon,
-          :xml => :xml_icon,
-          :rtf => :word_icon,
-          :msword => :word_icon,
-          :msexcel => :excel_icon,
-        },
-        :image => {
-          :default => :image_icon
-        },
-        :audio => {
-          :default => :audio_icon
-        },
-        :video => {
-          :default => :video_icon
-        },
-        :text => {
-          :default => :text_icon,
-          :css => :css_icon,
-          :html => :html_icon,
-          :rtf => :word_icon,
-          :richtext => :word_icon,
-          :xml => :xml_icon,
-          :comma_separated_values => :excel_icon
-        }
-      }
-    }
+    mattr_accessor :mime_mapping 
+    @@mime_mapping = ActiveSupport::OrderedHash.new()
+    @@mime_mapping[:document] = [/^application\//]
+    @@mime_mapping[:image]    = [/^image/]
+    @@mime_mapping[:audio]    = [/^audio/]
+    @@mime_mapping[:video]    = [/^video/]
+    @@mime_mapping[:text]     = [/^text/]
+    @@mime_mapping[:pdf]      = [/^(application\/)?pdf/]
+    @@mime_mapping[:archive]  = [/^(application\/)?(zip|gzip)/]
+    @@mime_mapping[:xml]      = [/^((application|text)\/)?xml/]
+    @@mime_mapping[:word]     = [/^(application\/)?(msword|rtf|vnd\.ms-word|vnd\.openxmlformats\-officedocument\.wordprocessingml)/]
+    @@mime_mapping[:excel]    = [/^(application\/)?(msexcel|vnd\.ms-excel|vnd\.openxmlformats\-officedocument\.spreadsheetml)/]
+    @@mime_mapping[:ppt]      = [/^(application\/)?(ppt|powerpoint|vnd\.ms-powerpoint|vnd\.openxmlformats\-officedocument\.presentationml)/]
+    @@mime_mapping[:html]     = [/^(text\/)?html/]
     
-    # Mapping of names to image assets
-    mattr_accessor :image_mapping
-    @@image_mapping = {
-      :spacer => "/images/fancygrid/spacer.gif"
-    }
+    mattr_accessor :default_size
+    @@default_size = "16x16"
     
-    # Mapping of names to image assets
-    mattr_accessor :icon_default_options
-    @@icon_default_options = {
-      :size => "16x16",
-      :alt => "icon"
-    } 
-    # Creates an image tag with an empty image source and the size option <tt>:size => 16x16</tt>.
+    mattr_accessor :default_alt_attribute
+    @@default_css_class = "icon"
+    
+    mattr_accessor :default_class_attribute
+    @@default_css_class = "default"
+    
+    mattr_accessor :additional_class_attribute
+    @@additional_class_attribute = "icon"
+    
+    # Creates an image tag with a transparent image
     #
     # === Example
     #
-    #    icon_tag(:class => :new_icon) 
-    #    # => <img src="/images/spacer.gif" class="new_icon" width="16" height="16" alt="spacer"/>
+    #    icon_tag
+    #    # => <img src="data:image/gif;base64,R0lGODlhAQABAIAAAMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw%3D%3D" class="default icon" width="16" height="16" alt="icon"/>
     #
-    # User the <tt>options</tt> attribute to override the default values for <tt>:size</tt> and <tt>:alt</tt>
+    #    icon_tag(:icon_name)
+    #    # => <img src="data..." class="icon_name icon" width="16" height="16" alt="icon"/>
     #
-    def icon_tag( options={} )
-      image_tag( get_image_path(:spacer), FancyTools::IconHelper.icon_default_options.merge(options) )
+    #    # the following method calls are equivalent
+    #    icon_tag(:class => "foo bar")
+    #    icon_tag(:foo, :class => :bar)
+    #    icon_tag(:foo, :bar)
+    #    icon_tag(:foo, :bar, :icon)
+    #    # => <img src="data..." class="foo bar icon" width="16" height="16" alt="icon"/>
+    #
+    #    icon_tag(:icon_name, :alt => "alt attribute")
+    #    # => <img src="data..." class="icon_name icon" width="16" height="16" alt="alt attribute"/>
+    #
+    #    icon_tag(:icon_name, :size => 10)
+    #    icon_tag(:icon_name, :size => "10x10")
+    #    # => <img src="data..." class="icon_name icon" width="10" height="10" alt="icon"/>
+    #
+    def icon_tag(*args)      
+      options = args.extract_options!
+      options.symbolize_keys!
+      options[:title] ||= options[:alt]
+      options[:alt] ||= (options[:title] || IconHelper.default_alt_attribute)
+      options[:src] ||= "data:image/gif;base64,R0lGODlhAQABAIAAAMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw%3D%3D"
+      if (size = (options.delete(:size) || IconHelper.default_size).to_s)
+        options[:width], options[:height] = size.split("x") if size =~ /^\d+x\d+$/
+        options[:width] = options[:height] = size if size =~ /^\d+$/
+      end
+      
+      css_class = args + options[:class].to_s.split(" ")
+      if css_class.empty?
+        css_class = [IconHelper.default_class_attribute]
+      end
+      css_class << IconHelper.additional_class_attribute
+      options[:class] = css_class.compact.uniq.join(" ")
+      tag("img", options)
     end
     
-    # Creates an <tt>icon_tag</tt> using the <tt>name</tt> for the <tt>:class</tt> option
-    #
-    def named_icon( name, options={} )
-      icon_tag( options.merge(:class => name) )
-    end
-    
-    # Creates an <tt>icon_tag</tt> using the <tt>name</tt> to search for a css class
-    # for the <tt>:class</tt> option
-    # See <tt>find_css_for_icon</tt> method for more details
-    #
-    def action_icon( name, options={} )
-      css = get_css_class([:action, name])
-      icon_tag( options.merge(:class => css) )
-    end
-    
-    # Shortcut for <tt>action_icon(:new)</tt>
-    #
+    # Shortcut for <tt>icon_tag(:action, :new, options)</tt>
     def new_icon( options={} )
-      action_icon(:new, options)
+      icon_tag(:new, options)
     end
     
-    # Shortcut for <tt>action_icon(:add)</tt>
-    #
+    # Shortcut for <tt>icon_tag(:action, :add, options)</tt>
     def add_icon( options={} )
-      action_icon(:add, options)
+      icon_tag(:add, options)
     end
        
-    # Shortcut for <tt>action_icon(:add_photo)</tt>
-    #
+    # Shortcut for <tt>icon_tag(:action, :add_photo, options)</tt>
     def add_photo_icon( options={} )
-      action_icon(:add_photo, options)
+      icon_tag(:add_photo, options)
     end
      
-    # Shortcut for <tt>action_icon(:show)</tt>
-    #
+    # Shortcut for <tt>icon_tag(:action, :show, options)</tt>
     def show_icon( options={} )
-      action_icon(:show, options)
+      icon_tag(:show, options)
     end
     
-    # Shortcut for <tt>action_icon(:edit)</tt>
-    #
+    # Shortcut for <tt>icon_tag(:action, :edit, options)</tt>
     def edit_icon( options={} )
-      action_icon(:edit, options)
+      icon_tag(:edit, options)
     end
     
-    # Shortcut for <tt>action_icon(:delete)</tt>
-    #
+    # Shortcut for <tt>icon_tag(:action, :delete, options)</tt>
     def delete_icon( options={} )
-      action_icon(:delete, options)
+      icon_tag(:delete, options)
     end
     
     # Creates an icon for the given mime type name.
-    def mime_icon( mime_type, options={} )
-      if mime_type.to_s.match(/\//)
-        mime_type = [:mime, mime_type.to_s.split("/")] 
-      else
-        mime_type = [:mime, :default]
+    def mime_icon( mime, options={} )
+      name = :document
+      IconHelper.mime_mapping.keys.each do |key|
+        name = key if IconHelper.mime_mapping[key].any? { |regex| mime.to_s =~ regex }
       end
-    
-      css = get_css_class(mime_type)
-      icon_tag( options.merge(:class => css) )
+      icon_tag(name, options )
     end
-    
-    def get_css_class( args )
-      get_from_hash(FancyTools::IconHelper.class_mapping, args)
-    end
-    
-    def get_image_path( args )
-      get_from_hash(FancyTools::IconHelper.image_mapping, args)
-    end
-    
-    def get_from_hash(hash, args)#:nodoc:
-      last = hash
-      args = [args] if (args.kind_of?(Symbol) || args.kind_of?(String))
-      args = args.flatten
-      args.each do |name|
-        name = name.to_s.gsub("-", "_")
-        next if( name.length == 0 )
-        
-        temp = ( last[name] or last[name.to_sym] )
-        return last[:default] unless temp
-        return temp unless temp.is_a?(Hash)
-        last = temp
-      end
-      
-      return last.to_s
-    end
+
   end
 end
 
